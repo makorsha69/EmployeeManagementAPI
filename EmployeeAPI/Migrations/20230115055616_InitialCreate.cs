7fd79de3869d4a8a260bcfe4bead2aa9daf7da6c
Saumyadip Mukherjee <soumyadip1998mukherjee@gmail.com>
2023-01-17 12:32:59 +0530

    Add project files.

---
+++ EmployeeAPI/Migrations/20230115055616_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmpID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Address = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Mobile = table.Column<string>(nullable: true),
+                    Salary = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    Department = table.Column<string>(type: "varchar(15)", nullable: true),
+                    DOJ = table.Column<DateTime>(nullable: false),
+                    isActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmpID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "ID will be automatically generated")]
+        public int EmpID { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required(ErrorMessage = "The train name cannot be empty.")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required(ErrorMessage = "The email cannot be empty.")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [Required(ErrorMessage = "The addressname cannot be empty.")]
+        public string Address { get; set; }
+
+
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Please enter valid phone no.")]
+        public string Mobile { get; set; }
+
+        [Column(TypeName = "numeric(10,2)")]
+        [Required(ErrorMessage = "Salary is Compulsory.")]
+        public int Salary { get; set; }
+
+        [Column(TypeName = "varchar(15)")]
+        public string Department { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "MM/DD/YYYY Format")]
+        public DateTime DOJ { get; set; }
+
+        public bool isActive { get; set; }
+
+    }
+}
